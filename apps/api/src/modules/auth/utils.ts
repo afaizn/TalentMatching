87f0a4079ae7885139397bbb47a2491efd24697f
Jhorgi <jhorgigheovani@gmail.com>
2026-02-18 20:53:02 +0700

    api auth

--- apps/api/src/modules/auth/utils.ts
+++ apps/api/src/modules/auth/utils.ts
@@ -11,17 +11,29 @@ export function comparePassword(password: string, hashPassword: string) {
 }
 
 export function generateAcessToken(userId: number) {
+	const secret = process.env.JWT_SECRET;
+	if (!secret) {
+		throw new Error("JWT_SECRET is not defined");
+	}
 	return jwt.sign({ sub: userId, type: "access" }, process.env.JWT_SECRET!, {
 		expiresIn: "5m",
 	});
 }
 
 export function generateRefreshToken(userId: number) {
+	const secret = process.env.JWT_SECRET;
+	if (!secret) {
+		throw new Error("JWT_SECRET is not defined");
+	}
 	return jwt.sign({ sub: userId, type: "refresh" }, process.env.JWT_SECRET!, {
 		expiresIn: "7d",
 	});
 }
 
 export function verifyRefreshToken(token: string) {
-	return jwt.verify(token, process.env.JWT_SECRET!);
+	const secret = process.env.JWT_SECRET;
+	if (!secret) {
+		throw new Error("JWT_SECRET is not defined");
+	}
+	return jwt.verify(token, secret);
 }
